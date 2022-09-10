1c0ad633664210a5570c81b24da6942d83515850
Mark Harfouche <mark.harfouche@gmail.com>
2022-09-10 13:14:59 -0400

    Revert "Merge pull request #24 from h-vetinari/bump"
    This reverts commit 8e7626b8826dcd31b0576a92cb76f0b7da36ae89, reversing
    changes made to ae96e4ef0d7c3948f491641671c891171ce0de7a.

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -24,10 +24,7 @@ export CONFIG_FILE="${CI_SUPPORT}/${CONFIG}.yaml"
 cat >~/.condarc <<CONDARC
 
 conda-build:
-  root-dir: ${FEEDSTOCK_ROOT}/build_artifacts
-pkgs_dirs:
-  - ${FEEDSTOCK_ROOT}/build_artifacts/pkg_cache
-  - /opt/conda/pkgs
+ root-dir: ${FEEDSTOCK_ROOT}/build_artifacts
 
 CONDARC
 
@@ -48,10 +45,6 @@ make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
 
 ( endgroup "Configuring conda" ) 2> /dev/null
-
-if [[ -f "${FEEDSTOCK_ROOT}/LICENSE.txt" ]]; then
-  cp "${FEEDSTOCK_ROOT}/LICENSE.txt" "${RECIPE_ROOT}/recipe-scripts-license.txt"
-fi
 
 if [[ "${BUILD_WITH_CONDA_DEBUG:-0}" == 1 ]]; then
     if [[ "x${BUILD_OUTPUT_ID:-}" != "x" ]]; then
