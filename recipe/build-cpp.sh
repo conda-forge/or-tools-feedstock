93eb60e42cbf7f5219dd8fc88899038b37d638e1
H. Vetinari <h.vetinari@gmx.com>
2023-02-15 22:13:05 +1100

    ensure we're using C++17

--- recipe/build-cpp.sh
+++ recipe/build-cpp.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+set -ex
 
 cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_CXX_STANDARD=17 \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DBUILD_SHARED_LIBS=ON \
       -BUILD_DEPS=OFF \
