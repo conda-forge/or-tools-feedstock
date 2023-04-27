eba43385a1779e276b7d05c422995cc283bf5fd3
H. Vetinari <h.vetinari@gmx.com>
2023-04-27 18:35:31 +1100

    normalize CMake invocation

--- recipe/build-cpp.sh
+++ recipe/build-cpp.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 set -ex
 
-cmake -G Ninja \
-      ${CMAKE_ARGS} \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_CXX_STANDARD=17 \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DBUILD_SHARED_LIBS=ON \
-      -BUILD_DEPS=OFF \
-      -DUSE_SCIP=OFF \
-      -S. \
-      -Bbuild \
-      -DBUILD_SAMPLES=OFF \
-      -DBUILD_EXAMPLES=OFF
+mkdir build
+cd build
 
-cmake --build build --target install -j${CPU_COUNT}
+cmake -G Ninja \
+    ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_DEPS=OFF \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_SAMPLES=OFF \
+    -DUSE_SCIP=OFF \
+    ..
+
+cmake --build . --target install -j${CPU_COUNT}
