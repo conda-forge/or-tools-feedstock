a5f50926b2668d0b9871278cfdebdd1b195cd7ca
H. Vetinari <h.vetinari@gmx.com>
2023-05-30 13:53:39 +1100

    add libprotobuf for cross-compilation, point to it

--- recipe/build-python.sh
+++ recipe/build-python.sh
@@ -6,6 +6,10 @@ rm -rf build
 mkdir build
 cd build
 
+if [[ "${target_platform}" != "${build_platform}" ]]; then
+    CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc"
+fi
+
 cmake -G Ninja \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,10 @@ set -ex
 mkdir build
 cd build
 
+if [[ "${target_platform}" != "${build_platform}" ]]; then
+    CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc"
+fi
+
 cmake -G Ninja \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
