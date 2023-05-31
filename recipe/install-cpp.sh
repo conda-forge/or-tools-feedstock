2f9d840921ae879889e862d23cf1656fc4407fdd
H. Vetinari <h.vetinari@gmx.com>
2023-05-31 21:50:51 +1100

    add output ortools-samples, test scip integration

--- recipe/install-cpp.sh
+++ recipe/install-cpp.sh
@@ -7,7 +7,16 @@ if [[ "$PKG_NAME" == "libortools" ]]; then
     # install only library components & headers
     cp -R ./temp_prefix/lib/* $PREFIX/lib
     cp -R ./temp_prefix/include/* $PREFIX/include
+    rm -rf temp_prefix
+elif [[ "$PKG_NAME" == "ortools-cpp" ]]; then
+    mkdir temp_prefix
+    cmake --install ./build --prefix=./temp_prefix
+    # install selected binaries & share
+    cp -R ./temp_prefix/share/* $PREFIX/share
+    cp ./temp_prefix/bin/fzn-ortools $PREFIX/bin
+    cp ./temp_prefix/bin/sat_runner $PREFIX/bin
+    cp ./temp_prefix/bin/solve $PREFIX/bin
 else
-    # install everything else (binaries/share ...)
+    # install everything else (samples)
     cmake --install ./build --prefix=$PREFIX
 fi
